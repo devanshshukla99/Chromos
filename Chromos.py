5c2fb12ddcbea142d99531c53d707298a4d652c9
devanshshukla99 <48386249+devanshshukla99@users.noreply.github.com>
2020-09-08 17:28:44 +0530

    Chromos 2.0

--- Chromos.py
+++ Chromos.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
 #Colored Text Model
 
+class InvalidColor(Exception):
+    """
+    Exception Raise when Invalid Color is entered.
+    """
+    def __init__(self, message="Invalid Color"):
+        self.message = message
+
+        super().__init__(self.message)
+
+    pass
+
 class Chromos():
+    """
+    Chromos provides colored-text terminal output.
+    """
 
     def __init__(self):
         super(Chromos, self).__init__()
@@ -52,17 +66,27 @@ class Chromos():
 
         if(self.no_style==False):
             if(self.bold==True):
-                self.attr += "1;"
+                self.attr = ''.join([self.attr, "1;"])
             if(self.underline==True):
-                self.attr += "2;"
+                self.attr = ''.join([self.attr, "2;"])
             if(self.negative1==True):
-                self.attr += "3;"
+                self.attr = ''.join([self.attr, "3;"])
             if(self.negative2==True):
-                self.attr += "5;"
+                self.attr = ''.join([self.attr, "5;"])
         else:
-            self.attr +=  "0;"
+            self.attr = ''.join([self.attr, "0;"])
 
         return
+
+    def cstr(self, color, string):
+        
+        self.get_attr()
+
+        try:
+            return ''.join([self.attr, self.txt_colors[color.lower()], "m", string])
+        except KeyError:
+            raise InvalidColor
+
 
     def blue(self, string):
 
