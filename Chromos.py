e403484ab3d09750133e687b0049342ba7a60e59
devanshshukla99 <48386249+devanshshukla99@users.noreply.github.com>
2020-10-17 13:32:56 +0530

    Attr Interpret

--- Chromos.py
+++ Chromos.py
@@ -12,9 +12,27 @@ class InvalidColor(Exception):
 
     pass
 
+class InvalidAttribute(Exception):
+    """
+    Exception Raised when Invalid Attribute is entered.
+    """
+    def __init__(self, message="Invalid Attribute"):
+        self.message = message
+
+        super().__init__(self.message)
+
+    pass
+
 class Chromos():
     """
     Chromos provides colored-text terminal output.
+
+    Styles:
+        Bold
+        Itallic         (may not be supported)
+        Underline
+        Blink           (may not be supported)
+        Strikethrough
     """
 
     def __init__(self):
@@ -24,9 +42,10 @@ class Chromos():
 
         self.no_style = False
         self.bold = True
+        self.itallic = False
         self.underline = False
-        self.negative1 = False
-        self.negative2 = False
+        self.blink = False
+        self.strikethrough = False
         self.attr = self.base_attr
 
         self.txt_colors = {
@@ -54,9 +73,10 @@ class Chromos():
         self.style = {
             "no_style":"0",
             "bold":"1",
-            "underline":"2",
-            "negative1":"3",
-            "negative2":"5"
+            "itallic": "3",
+            "underline":"4",
+            "blink": "5",
+            "strikethrough": "9"
 
         }
     
@@ -67,12 +87,14 @@ class Chromos():
         if(self.no_style==False):
             if(self.bold==True):
                 self.attr = ''.join([self.attr, "1;"])
-            if(self.underline==True):
-                self.attr = ''.join([self.attr, "2;"])
-            if(self.negative1==True):
+            if(self.itallic==True):
                 self.attr = ''.join([self.attr, "3;"])
-            if(self.negative2==True):
+            if(self.underline==True):
+                self.attr = ''.join([self.attr, "4;"])
+            if(self.blink==True):
                 self.attr = ''.join([self.attr, "5;"])
+            if(self.strikethrough==True):
+                self.attr = ''.join([self.attr, "9;"])
         else:
             self.attr = ''.join([self.attr, "0;"])
 
@@ -94,7 +116,68 @@ class Chromos():
             return ''.join([self.attr, self.txt_colors[color.lower()], "m", string, "\033[0m"])
         except KeyError:
             raise InvalidColor
+    
+    def attribute_intrepretor(self, args):
+        """
+        bl  --  blink
+        it  --  itallic
+        u   --  underline
+        blk --  blink
+        st  --  strikethrough
+        """
+        attrinter = {
+                "bf": "bold",
+                "it": "itallic",
+                "u": "underline",
+                "blk": "blink",
+                "st": "strikethrough"
+                # "bold": "bold",
+                # "itallic": "itallic",
+                # "underline": "underline",
+                # "blink": "blink",
+                # "strikethrough": "strikethrough"
+                }
 
+        args = args[0].split(" ")
+        attr = self.base_attr
+
+        for arg in args:
+
+            if(len(arg.lower()) > 3):
+                attr = ''.join([attr, self.style[arg.lower()], ";"])
+            else:
+                attr = ''.join([attr, self.style[attrinter[arg.lower()]], ";"])
+
+        return attr
+
+    def cstrattr(self, args):
+        """
+        Parameters:
+            args[0]: color (str): Color.
+            args[1:-1]: Attributes
+            args[-1]: string (str): Text.
+
+        Returns:
+            str: Text with Appropiate Prefix and Suffix.
+        """
+
+        color = args[0]
+        string = args[-1]
+
+        args.pop(0)
+        args.pop(-1)
+
+        try:
+            attr = self.attribute_intrepretor(args)
+        except KeyError:
+            raise InvalidAttribute
+
+        try:
+            return ''.join([attr, self.txt_colors[color.lower()], "m", string, "\033[0m"])
+        except KeyError:
+            raise InvalidColor
+
+    # Preserving Individual Funcs for Compatibility Reasons
 
     def blue(self, string):
 
