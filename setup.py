c3acb906428a7fb0a4fb1d4ef37a262043783208
Devansh Shukla <48386249+devanshshukla99@users.noreply.github.com>
2021-03-03 22:19:14 +0530

    v2.2

--- setup.py
+++ setup.py
@@ -1,25 +1,26 @@
 
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
- 
-setuptools.setup(
-     name='Chromos',
-     version='2.0',
-     scripts=['Chromos.py'] ,
-     author="devanshshukla99",
-     description="Package for getting colored text output in Python terminal",
-     long_description= long_description,
-     long_description_content_type="text/markdown",
-     url="https://github.com/devanshshukla99/Chromos",
-     license = "MIT LICENCE",
-     classifiers=[
-         "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: MIT License",
-         "Operating System :: OS Independent",
-     ],
- )
 
+setup(
+    name = 'Chromos',
+    version = '2.2',
+    scripts=['src/Chromos/Chromos.py'],
+    setup_requires = ['pytest-runner',],
+    tests_require = ['pytest',],
+    author = "devanshshukla99",
+    description = "Package for getting colored text output in Python terminal",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/devanshshukla99/Chromos",
+    license = "MIT LICENCE",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+ )
 
 # python3 setup.py bdist_wheel
