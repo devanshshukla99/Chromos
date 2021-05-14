c3f1a767f6b5c8b2bbaf47fab161c0520afb0b88
devanshshukla99 <48386249+devanshshukla99@users.noreply.github.com>
2021-05-14 22:40:04 +0530

    fixed + added tox

--- Chromos/_Chromos.py
+++ Chromos/_Chromos.py
@@ -55,40 +55,37 @@ class Chromos():
     blink = False
     strikethrough = False
     colors = {
-            "black": "30",
-            "red": "31",
-            "green": "32",
-            "yellow": "33",
-            "blue": "34",
-            "purple": "35",
-            "cyan": "36",
-            "white": "37",
-            "bgblack": "40",
-            "bgred": "41",
-            "bggreen": "42",
-            "bgyellow": "43",
-            "bgblue": "44",
-            "bgpurple": "45",
-            "bgcyan": "46",
-            "bgwhite": "47"
-        }
+        "black": "30",
+        "red": "31",
+        "green": "32",
+        "yellow": "33",
+        "blue": "34",
+        "purple": "35",
+        "cyan": "36",
+        "white": "37",
+        "bgblack": "40",
+        "bgred": "41",
+        "bggreen": "42",
+        "bgyellow": "43",
+        "bgblue": "44",
+        "bgpurple": "45",
+        "bgcyan": "46",
+        "bgwhite": "47"}
 
     style = {
-            "no_style": "0",
-            "bold": "1",
-            "itallic": "3",
-            "underline": "4",
-            "blink": "5",
-            "strikethrough": "9"
-        }
+        "no_style": "0",
+        "bold": "1",
+        "itallic": "3",
+        "underline": "4",
+        "blink": "5",
+        "strikethrough": "9"}
 
     attrinter = {
-            "bf": "bold",
-            "it": "itallic",
-            "u": "underline",
-            "blk": "blink",
-            "st": "strikethrough"
-        }
+        "bf": "bold",
+        "it": "itallic",
+        "u": "underline",
+        "blk": "blink",
+        "st": "strikethrough"}
 
     def get_attr(self):
         self.attr = self.base_attr
--- tests/test_Chromos.py
+++ tests/test_Chromos.py
@@ -2,7 +2,7 @@
 
 import pytest
 
-from Chromos import Chromos
+from Chromos._Chromos import Chromos
 
 
 @pytest.fixture
@@ -11,33 +11,41 @@ def bare_chromos():
     """
     return Chromos()
 
+
 def test_bare_chromos_black(bare_chromos):
     result = bare_chromos.black('Hello!!')
     assert result == '\x1b[1;30mHello!!\x1b[0m'
+
 
 def test_bare_chromos_red(bare_chromos):
     result = bare_chromos.red('Hello!!')
     assert result == '\x1b[1;31mHello!!\x1b[0m'
 
+
 def test_bare_chromos_green(bare_chromos):
     result = bare_chromos.green('Hello!!')
     assert result == '\x1b[1;32mHello!!\x1b[0m'
+
 
 def test_bare_chromos_yellow(bare_chromos):
     result = bare_chromos.yellow('Hello!!')
     assert result == '\x1b[1;33mHello!!\x1b[0m'
 
+
 def test_bare_chromos_blue(bare_chromos):
     result = bare_chromos.blue('Hello!!')
     assert result == '\x1b[1;34mHello!!\x1b[0m'
+
 
 def test_bare_chromos_purple(bare_chromos):
     result = bare_chromos.purple('Hello!!')
     assert result == '\x1b[1;35mHello!!\x1b[0m'
 
+
 def test_bare_chromos_cyan(bare_chromos):
     result = bare_chromos.cyan('Hello!!')
     assert result == '\x1b[1;36mHello!!\x1b[0m'
+
 
 def test_bare_chromos_white(bare_chromos):
     result = bare_chromos.white('Hello!!')
